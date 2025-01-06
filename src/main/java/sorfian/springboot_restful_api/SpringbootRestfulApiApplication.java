6d80dcb0af0d9be8c9696ab711dce4a61714f619
Sorfian <sorfian.tkj@gmail.com>
2025-01-06 12:21:17 +0800

    initial commit

---
+++ src/main/java/sorfian/springboot_restful_api/SpringbootRestfulApiApplication.java
@@ -0,0 +1,13 @@
+package sorfian.springboot_restful_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestfulApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sorfian/springboot_restful_api/SpringbootRestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package sorfian.springboot_restful_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
